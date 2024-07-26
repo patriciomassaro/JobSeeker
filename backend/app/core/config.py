6181eb75c320d5f8d42e138a8602016a94def8b8
Patricio Massaro <35375828+patriciomassaro@users.noreply.github.com>
2024-07-26 18:15:44 -0400

    add dev as environment

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -32,7 +32,7 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     DOMAIN: str = "localhost"
-    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+    ENVIRONMENT: Literal["local", "dev", "production"] = "local"
 
     @computed_field  # type: ignore[misc]
     @property
